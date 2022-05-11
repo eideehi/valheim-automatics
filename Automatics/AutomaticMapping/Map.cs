cd495803653bb72ba9010dd1d8ff1550346f6687
eideehi <account@eidee.net>
2022-05-11 23:21:05 +0900

    Change to search 'custom-map-icon.json' from BepinEx plugin folder as well

--- Automatics/AutomaticMapping/Map.cs
+++ Automatics/AutomaticMapping/Map.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using Automatics.ModUtils;
+using BepInEx;
 using LitJson;
 using UnityEngine;
 
@@ -29,8 +30,17 @@ namespace Automatics.AutomaticMapping
             Deprecated.Map.Initialize();
             CustomIcons.Clear();
             LoadCustomIcons(Automatics.GetDefaultResourcePath("Textures"));
+            LoadCustomIconMods();
             LoadCustomIcons(Automatics.GetInjectedResourcePath("Textures"));
             RegisterCustomIcons();
+        }
+
+        private static void LoadCustomIconMods()
+        {
+            foreach (var directory in Directory.GetDirectories(Paths.PluginPath))
+            {
+                LoadCustomIcons(Path.Combine(directory, "Textures"));
+            }
         }
 
         private static void LoadCustomIcons(string texturesDir)
